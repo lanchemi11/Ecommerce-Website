fa86e46e492fe52819d3e7d56df9bdb9f07d9603
Nixyz11 <nikola.bogdanovic.ssfon@gmail.com>
2023-01-22 14:51:50 +0100

    added

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// Script for navigation bar
+const bar = document.getElementById("bar");
+const closed = document.getElementById("close");
+const nav = document.getElementById("navbar");
+
+if (bar) {
+  bar.addEventListener("click", () => {
+    nav.classList.add("active");
+  });
+}
+
+if (closed) {
+  closed.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
+}
